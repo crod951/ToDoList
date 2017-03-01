c1e33be387cbc8f827a46ed1f66aecf40dda80bf
Chris Rodrigues <crod951@gmail.com>
2017-02-28 15:40:39 -1000

    Creating modal for creating topic

--- src/app/modules/list/ListController.js
+++ src/app/modules/list/ListController.js
@@ -1,9 +1,10 @@
 (function () {
-  function ListController() {
+  function ListController($uibModal) {
     var vm = this;
     vm.numTasks = 0;
     vm.taskList = []
     vm.todayDate = moment().format('ddd MMM Do YYYY');
+    vm.selectedTopic = {};
     vm.setCreatingValue = false;
     vm.topics = [
       {"name":"Personal"},
@@ -18,9 +19,21 @@
     vm.setNotCreating = function() {
       vm.setCreatingValue = false;
     }
+
+    vm.checkCreateNewTopic = function() {
+      if (vm.selectedTopic.name === "Create New Topic") {
+        $uibModal.open({
+          animation: false,
+          backdrop: 'static',
+          keyboard: false,
+          templateUrl: 'app/modules/topic/CreateTopicView.html',
+          size: 'md'
+        });
+      }
+    }
   }
   angular
     .module('ToDoList')
-    .controller('ListController', [ListController]);
+    .controller('ListController', ['$uibModal', ListController]);
 
 })();
